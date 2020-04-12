3a8432349c8a1be051b4423ed30623c448e4d5ae
杨智麟 <yang239246@163.com>
2020-04-12 21:51:42 +0800

    modify some paramname

--- src/main/java/cn/chilin/algorithm/QuickSort.java
+++ src/main/java/cn/chilin/algorithm/QuickSort.java
@@ -15,34 +15,34 @@ public class QuickSort {
         sort(nums, 0, nums.length - 1);
     }
 
-    private static void sort(int[] nums, int left, int right) {
-        if (left >= right) return;
-        int p = partition(nums, left, right);
-        sort(nums, left, p - 1);
-        sort(nums, p + 1, right);
+    private static void sort(int[] nums, int lo, int hi) {
+        if (lo >= hi) return;
+        int p = partition(nums, lo, hi);
+        sort(nums, lo, p - 1);
+        sort(nums, p + 1, hi);
     }
 
-    private static int partition(int[] nums, int left, int right) {
-        int i = left;
-        int j = right + 1;
-        int v = nums[left];
+    private static int partition(int[] nums, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1; // 因为右侧扫描是先自减，这里+1处理一下
+        int v = nums[lo];
         // 直到i,j相遇跳出循环
         while (true) {
             // 当遇到比v大的数，跳出来
             while (nums[++i] < v) {
-                if (i == right) break;
+                if (i == hi) break;
             }
             // 当遇到比v小的数，跳出
             while (nums[--j] > v) {
-                if (j == left) break;
+                if (j == lo) break;
             }
             // 先判断边界，在执行交换
             if (i >= j) break;
             exch(nums, i, j);
         }
-        // 跳出的时候，因为 i>=j，所以使用j与left交换，将切分值留在nums[j]
-        exch(nums, left, j);
-        // 此时实现： nums[left..j-1] < nums[j] < nums[j + 1..right]
+        // 跳出的时候，因为 i>=j，所以使用j与lo交换，将切分值留在nums[j]
+        exch(nums, lo, j);
+        // 此时实现： nums[lo..j-1] < nums[j] < nums[j + 1..hi]
         return j;
     }
 
