216a81de9290136ecd9d8e420695c6bc41c47217
杨智麟 <37733050+chirinnyo@users.noreply.github.com>
2020-07-02 16:48:36 +0800

    Create ReverseStackUsingRecursive.java

---
+++ src/main/java/cn/chilin/algorithm/ReverseStackUsingRecursive.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class ReverseStackUsingRecursive {
+ 
+	public static void reverse(Stack<Integer> stack) {
+		if (stack.isEmpty()) return;
+		int i = getAndRemoveLastElement(stack);
+		reverse(stack);
+		stack.push(i);
+	}
+
+
+	public static int getAndRemoveLastElement(Stack<Integer> stack) {
+		int result = stack.pop();
+		if (stack.isEmpty()) {
+			return result;
+		} else {
+			int last = getAndRemoveLastElement(stack);
+			stack.push(result);
+			return last;
+		}
+	}
+
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		reverse(stack);
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+	}
+
+}
